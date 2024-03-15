39eabbb5382b7487cb59fed057e55c4d6839e5aa
laposhko <laposhko0@gmail.com>
2024-03-15 15:39:52 +0200

    fixed paddings in mobile + header

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 // create your functions in separate .js files in js folder and import here
+
+// header menu dropdown
+const menuBtn = document.querySelector('.menu-btn');
+const headerMenu = document.querySelector('.header-menu');
+menuBtn.addEventListener('click', () => {
+  headerMenu.classList.toggle('visually-hidden');
+});
