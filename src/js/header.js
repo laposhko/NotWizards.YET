aa4aadbd693673db1f5c9224b5eb95329df238c7
laposhko <laposhko0@gmail.com>
2024-03-16 22:56:30 +0200

    fixed container width

--- src/js/header.js
+++ src/js/header.js
@@ -2,12 +2,15 @@
 const headerBtn = document.querySelector('.header-btn');
 const backdrop = document.querySelector('.backdrop');
 headerBtn.addEventListener('click', () => {
-  backdrop.classList.remove('visually-hidden');
+  backdrop.classList.remove('backdrop--hide');
+
+  backdrop.classList.add('backdrop--show');
 });
 // mobile menu closing
 const mobmenuCloseBtn = document.querySelector('.mobmenu__close');
 mobmenuCloseBtn.addEventListener('click', () => {
-  backdrop.classList.add('visually-hidden');
+  backdrop.classList.remove('backdrop--show');
+  backdrop.classList.add('backdrop--hide');
 });
 // tablet/desktop menu
 const menuBtn = document.querySelector('.menu-btn');
@@ -15,3 +18,15 @@ const headerMenu = document.querySelector('.header-menu');
 menuBtn.addEventListener('click', () => {
   headerMenu.classList.toggle('visually-hidden');
 });
+// smooth scroll
+const anchors = document.querySelectorAll('.header-menu-link');
+for (const anchor of anchors) {
+  anchor.addEventListener('click', event => {
+    event.preventDefault();
+    const blockID = anchor.getAttribute('href');
+    const wishedSection = document.getElementById(blockID);
+    wishedSection.scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+}
