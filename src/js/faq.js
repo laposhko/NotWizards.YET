8eeea88b7f797f696792a3a9206d84a2ec36c368
laposhko <92787511+laposhko@users.noreply.github.com>
2024-03-18 14:31:45 +0200

    Update faq.js
    test faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,5 +1,5 @@
 import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
+import 'accordion-js/dist/accordion.min.css';
 
 const accordions = Array.from(
   document.querySelectorAll('.accordion-container')
