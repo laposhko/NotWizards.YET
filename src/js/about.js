dd8972c385d11763062c8abcf4b8ede05b23c827
laposhko <laposhko0@gmail.com>
2024-03-19 11:37:29 +0200

    trying to fix about arrows

--- src/js/about.js
+++ src/js/about.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 ////////////////////////ACCORDION CODE//////////////////////////////
 
@@ -9,7 +9,9 @@ const accordionContainer = Array.from(
   document.querySelectorAll('.container-about')
 );
 
-const accordions = accordionContainer.map(container => container.querySelector('.accordion-list'));
+const accordions = accordionContainer.map(container =>
+  container.querySelector('.accordion-list')
+);
 
 new Accordion(accordions, {
   showMultiple: true,
@@ -18,20 +20,20 @@ new Accordion(accordions, {
   panelClass: 'accordion-description',
   openOnInit: [0],
   onOpen: function (currentElement) {
-      currentElement.querySelector('button').classList.add('active');
-      const arrowIcon = currentElement.querySelector('.arrow use');
-      arrowIcon.setAttribute('href', './img/symbol-defs.svg#icon-arrow-up')
-
-    },
-    onClose: function (currentElement) {
-        currentElement.querySelector('button').classList.remove('active');
-        const arrowIcon = currentElement.querySelector('.arrow use');arrowIcon.setAttribute('href', './img/symbol-defs.svg#icon-arrow-down');
-    },
+    currentElement.querySelector('button').classList.add('active');
+    const arrowIcon = currentElement.querySelector('.arrow use');
+    arrowIcon.setAttribute('href', '/img/symbol-defs.svg#icon-arrow-up');
+  },
+  onClose: function (currentElement) {
+    currentElement.querySelector('button').classList.remove('active');
+    const arrowIcon = currentElement.querySelector('.arrow use');
+    arrowIcon.setAttribute('href', '/img/symbol-defs.svg#icon-arrow-down');
+  },
 });
 
 const activeAccordion = document.querySelector('.accordion-element.is-active');
 const arrowIcon = activeAccordion.querySelector('.arrow use');
-arrowIcon.setAttribute('href', './img/symbol-defs.svg#icon-arrow-up');
+arrowIcon.setAttribute('href', '/img/symbol-defs.svg#icon-arrow-up');
 
 ////////////////////////SWIPER CODE//////////////////////////////
 
@@ -40,38 +42,36 @@ import 'swiper/css';
 import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 
 const swiperAbout = new Swiper('.skills-container', {
-    loop: true ,
-    direction: 'horizontal',
-    allowSlideNext: true,
-    setWrapperSize: true,
-    modules: [Navigation, Keyboard, Mousewheel],
-    simulateTouch: true,
-    grabCursor: true,
-    spaceBetween: 0,
-    speed: 1000,
-    navigation: {
-        nextEl: '.swipe-next-button',
-    },
-    breakpoints: {
+  loop: true,
+  direction: 'horizontal',
+  allowSlideNext: true,
+  setWrapperSize: true,
+  modules: [Navigation, Keyboard, Mousewheel],
+  simulateTouch: true,
+  grabCursor: true,
+  spaceBetween: 0,
+  speed: 1000,
+  navigation: {
+    nextEl: '.swipe-next-button',
+  },
+  breakpoints: {
     320: {
-        slidesPerView: 2,
-         
+      slidesPerView: 2,
     },
     375: {
-        slidesPerView: 2,
-    
+      slidesPerView: 2,
     },
     768: {
-        slidesPerView: 3,
+      slidesPerView: 3,
     },
     1440: {
-        slidesPerView: 6,
+      slidesPerView: 6,
     },
   },
-    keyboard: {
-        enabled: true,
-    },
-    mousewheel: {
-        invert: true,
-    },
+  keyboard: {
+    enabled: true,
+  },
+  mousewheel: {
+    invert: true,
+  },
 });
