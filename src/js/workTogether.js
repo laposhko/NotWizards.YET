6b659922ce07c2d006b4efac93a35f02caedbff2
laposhko <laposhko0@gmail.com>
2024-03-20 14:48:05 +0200

    modal icon

--- src/js/workTogether.js
+++ src/js/workTogether.js
@@ -59,12 +59,9 @@ const addNewUser = async event => {
     inst = basicLightbox.create(
       `<div class="modal">
             <button class="close-btn" id="close-btn" type="button">
-              // <svg class="close-btn-icon" width="22" height="22">
-              //   <use href="./img/symbol-defs.svg#icon-x"></use>
-              // </svg> 
-              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
-<path d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"></path>
-</svg>
+              <svg class="close-btn-icon" width="22" height="22">
+                <use href="https://github.com/laposhko/NotWizards.YET/blob/main/src/img/symbol-defs.svg#icon-x"></use>
+              </svg> 
             </button>
               <div class modal-message">
                 <h3 class="modal-title">${response.data.title}</h3>
