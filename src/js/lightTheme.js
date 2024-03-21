f26fd646dfb9212fcafb2d9f918ea8317a86f5e9
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-03-21 21:42:36 +0100

    new image

--- src/js/lightTheme.js
+++ src/js/lightTheme.js
@@ -5,4 +5,8 @@ switchButton.addEventListener('click', buttonSwitchHandler);
 function buttonSwitchHandler() {
   const body = document.querySelector('body');
   body.classList.toggle('light-theme');
+
+  const mobileMenuBg = document.querySelector('.backdrop');
+  mobileMenuBg.style.backgroundImage =
+    'url(url(/img/header/mobile-menu-white.png))';
 }
