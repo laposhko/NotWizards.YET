474b84ec6688634bf9f771702316f61d247fa3cf
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-03-21 23:27:16 +0100

    white bg

--- src/js/lightTheme.js
+++ src/js/lightTheme.js
@@ -6,6 +6,7 @@ function buttonSwitchHandler() {
   const body = document.querySelector('body');
   body.classList.toggle('light-theme');
 
-  const mobileMenuBg = document.querySelector('.backdrop');
-  mobileMenuBg.classList.toggle('hero-wite');
+  const pngForChange = document.querySelectorAll('.change');
+  pngForChange[0].classList.toggle('backdrop-white');
+  pngForChange[1].classList.toggle('hero-white');
 }
